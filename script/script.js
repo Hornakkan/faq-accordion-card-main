221f67a297e7b21cede20a3da80bea5f4112572e
Hornakkan <lacour.s@evhr.net>
2022-05-10 09:23:07 +0200

    remove consle log

--- script/script.js
+++ script/script.js
@@ -3,6 +3,5 @@ const element = document.querySelectorAll('.element');
 for(let i = 0; i < element.length; i++){
     element[i].addEventListener('click', (e) => {
         element[i].classList.toggle('active');
-        console.log(e.target);
     })
 }
